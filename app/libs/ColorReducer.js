90ecd5c97a7a46088068e1f8ccd5efe7556c44bc
Dan Kuck <dan@honeyfund.com>
2019-10-15 15:13:05 -0400

    In beautiful 27-color glory

--- app/libs/ColorReducer.js
+++ app/libs/ColorReducer.js
@@ -1,3 +1,34 @@
+/**
+ |--------------------
+ | ColorReducer
+ |--------------------
+ | Rounds the values in each RGB channel to a reduced set of values.
+ |
+ | `new ColorReducer(2)` would make each channel have 2 values, 0 and 255, so
+ | the 8 colors (2^3) would be:
+ | [  0,   0,   0] = black
+ | [  0,   0, 255] = blue
+ | [  0, 255,   0] = green
+ | [  0, 255, 255] = cyan
+ | [255,   0,   0] = red
+ | [255,   0, 255] = magenta
+ | [255, 255,   0] = yellow
+ | [255, 255, 255] = white
+ |
+ | `new ColorReducer(3)` would make each channel have 3 values, 0, 128, and
+ | 255, so the 27 (3^3) colors would be:
+ | [  0,   0,   0] = black          [128,   0,   0] = dark red        [255,   0,   0] = red
+ | [  0,   0, 128] = dark blue      [128,   0, 128] = dark pink       [255,   0, 128] = pink
+ | [  0,   0, 255] = blue           [128,   0, 255] = purple          [255,   0, 255] = magenta
+ |
+ | [  0, 128,   0] = dark green     [128, 128,   0] = dark yellow     [255, 128,   0] = leaf orange
+ | [  0, 128, 128] = dark cyan      [128, 128, 128] = grey            [255, 128, 128] = pastel pink
+ | [  0, 128, 255] = sky blue       [128, 128, 255] = light purple    [255, 128, 255] = bubble gum pink
+ |
+ | [  0, 255,   0] = green          [128, 255,   0] = bright green    [255, 255,   0] = yellow
+ | [  0, 255, 128] = light green    [128, 255, 128] = pastel gren     [255, 255, 128] = pastel yellow
+ | [  0, 255, 255] = cyan           [128, 255, 255] = light cyan      [255, 255, 255] = white
+ */
 
 export default class ColorReducer {
 
