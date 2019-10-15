c12d75253f1746a6bc640255f2a20c0c5bca5117
Dan Kuck <dan@honeyfund.com>
2019-10-15 17:14:56 -0400

    flops are easy to estimate

--- test/fixtures/flops.js
+++ test/fixtures/flops.js
@@ -5,24 +5,39 @@
  | Provides an estimate of this system's flops and a function to estimate the
  | operation count of a callback.
  */
-let time = 0;
-let ops = 500000;
 
-// retry this loop until time is at least 4 ms
+// The time we will measure
+let time = 1;
+// The operations we will perform
+let ops = 500000;
+// The minimum time we'll accept to ensure a good count
+let targetTime = 100;
+
+// Run op number of operations and see if it's at least targetTime.
+// If it's not, increase ops and try again.
 do {
-    ops = ops << 2; // double ops each time
+    // If the time is nowhere near the right time, only double the ops since we
+    // don't have any idea how to get close.
+    // If the time is pretty close to the right time, attempt to get just a
+    // little over the target time.
+    ops = time < targetTime / 2
+        ? ops << 2
+        : ops * (1.1 * targetTime / time);
     const before = new Date();
     for (let i = 0; i < ops; i++) {
         i % 1001;
     }
     const after = new Date();
     time = (after.valueOf() - before.valueOf());
-} while (time < 4);
+} while (time < targetTime);
 
+// Known floating point operations per measured millisecond
 const flopms = ops / time;
 
+// Export flops since that's what this library is called
 export default flopms * 1000;
 
+// Export this handy measuring function too
 export function estimateOperations(cb) {
     const start = new Date();
     cb();
