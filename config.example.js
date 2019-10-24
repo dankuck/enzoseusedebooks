b947594c922463bdb1e1102a9f27e89b7138b355
Dan Kuck <dan@honeyfund.com>
2019-10-24 16:26:22 -0400

    New config options

--- config.example.js
+++ config.example.js
@@ -1,3 +1,28 @@
 export default {
-    developmentMode: false,
+    /**
+     * Show the development tools button
+     * @type {Boolean}
+     */
+    developmentMode: true,
+
+    /**
+     * How long flash messages should be shown before going away or moving to
+     * the next message
+     * @type {Number} milliseconds
+     */
+    messagerSpeed: 2500,
+
+    /**
+     * How long hover names should be shown after the mouse leaves the hovered
+     * item. Hovering over a new item will immediately replace the message,
+     * disregarding this number.
+     * @type {Number} milliseconds
+     */
+    hovererLag: 250,
+
+    /**
+     * How long the auto-hover mechanism should linger on each item.
+     * @type {Number} milliseconds
+     */
+    autoHoverSpeed: 3000,
 };
